17772aabcde84708641072af56b93f6c89124e4b
Brennan Blair <jbb341@hotmail.com>
2018-08-28 18:08:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,7 @@
 function forLoop(array) {
  for (var i = 0; i < 25; i++) {
-   if (i === 0){
-   }
-   else if (i == 1) {
-     return ( "I am 1 strange loop.")
+   if (i == 1) {
+     return ( `I am ${i} strange loop.`)
    } else {
      return (`I am ${i} strange loops.`)
    }
