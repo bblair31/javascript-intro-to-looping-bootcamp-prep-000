4af3aff444fccef3a5f73a6df2c880c3a836137c
Brennan Blair <jbb341@hotmail.com>
2018-08-28 17:12:46 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array) {
+ for (var i = 0; i < 25; i++) {
+   if (i == 1) {
+     return ( "I am 1 strange loop.")
+   } else {
+     return (`I am ${i} strange loops.`)
+   }
+ }
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return "done"
+}
+
+function doWhileLoop(array) {
+  do {
+    array.length - 1
+  } while(array.length > 0)
+  return array
+}
